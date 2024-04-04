145bab777d4f56094b6383680da3b05fb5beb959
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-04-04 09:38:24 +0300

    tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {})
   // your code here
-});
+ let form = document.querySelector('form')
+ form.addEventListener('submit',(e) => {
+  e.preventDefault ()
+   buildToDo(e.target.new.value)
+   form.reset ()
+   
+ })
+
+ function buildToDo (to_do) {
+  let p = document.createElement("p")
+  let btn = document.createElement ("button")
+  btn.addEventListener("click",handleDelete)
+  btn.textContent=` x`
+  p.textContent = `${to_do}  `
+  document.querySelector("#tasks").appendChild(p)
+  p.appendChild(btn)
+
+ }
+
+ function handleDelete (e){
+  e.target.parentNode.remove() 
+ }
+
+ 
+
+
+ 
